0bae54ca8a3226c647d59e93e0521ca4bb8575de
Jakub Urbaniak <kuba121201@gmail.com>
2024-05-09 14:20:50 +0200

    add app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import Card from "./components/card/card";
 import Carusel from "./components/carusel/carusel";
 import Header from "./components/header/header";
 import Navbar from "./components/navbar/navbar";
+import "./App.scss";
 
 function App() {
   return (
-    <main>
+    <main className="main-section">
       <Header />
       <Navbar />
       <Carusel />
