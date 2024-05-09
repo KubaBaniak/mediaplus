7489f82433f491854e06689ab1a30747bc984735
Jakub Urbaniak <kuba121201@gmail.com>
2024-05-09 14:20:32 +0200

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,26 @@
+import "./header.scss";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+
 export default function Header() {
   return (
-    <nav>
-      <div>
+    <header className="top-header">
+      <div className="info">
         <div>About</div>
         <div>Doctors</div>
         <div>Contact</div>
         <div>FAQ</div>
       </div>
-      <div>+48517300252</div>
-      <div>j.urbaniak@moodup.team</div>
-    </nav>
+      <div className="contact">
+        <div>
+          <FaPhoneAlt />
+          +48517300252
+        </div>
+        <div>
+          <IoMdMail />
+          j.urbaniak@moodup.team
+        </div>
+      </div>
+    </header>
   );
 }
