f713ae577d52a2f85745bf6b19018edb1c65c810
Jakub Urbaniak <kuba121201@gmail.com>
2024-05-10 07:40:34 +0200

    sync

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,31 @@
+import "./navbar.scss";
+import { IoIosArrowDown } from "react-icons/io";
+
 export default function Navbar() {
   return (
-    <nav>
-      <img />
-      <div>
-        <div>Home</div>
+    <nav className="navigation">
+      <img
+        src="https://cdn.7tv.app/emote/613937fcf7977b64f644c0d2/4x.webp"
+        alt=""
+      />
+      <div className="nav-menu">
+        <div>
+          Home
+          <IoIosArrowDown />
+        </div>
         <div>Doctors</div>
         <div>Services</div>
-        <div>Pages</div>
-        <div>Blogs</div>
+        <div>
+          Pages
+          <IoIosArrowDown />
+        </div>
+        <div>
+          Blogs
+          <IoIosArrowDown />
+        </div>
         <div>Contact Us</div>
       </div>
-      <div>Book Appointment</div>
+      <button>Book Appointment</button>
     </nav>
   );
 }
