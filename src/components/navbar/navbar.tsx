62f32338370a7b4519e1ce25c8e67c7ffa836252
Jakub Urbaniak <kuba121201@gmail.com>
2024-05-10 15:36:46 +0200

    active navbar feature

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,27 +2,58 @@ import "./navbar.scss";
 import "../../reusables/buttons.scss";
 import { IoIosArrowDown } from "react-icons/io";
 import logo from "../../assets/img/logo.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [activeItem, setActiveItem] = useState("Home");
+
+  const handleNavMenuClick = (itemName: string) => {
+    setActiveItem(itemName);
+  };
+
   return (
     <nav className="navigation">
       <img src={logo} alt="" />
       <div className="nav-menu">
-        <div>
+        <div
+          onClick={() => handleNavMenuClick("Home")}
+          className={activeItem === "Home" ? "active" : ""}
+        >
           Home
           <IoIosArrowDown />
         </div>
-        <div>Doctors</div>
-        <div>Services</div>
-        <div>
+        <div
+          onClick={() => handleNavMenuClick("Doctors")}
+          className={activeItem === "Doctors" ? "active" : ""}
+        >
+          Doctors
+        </div>
+        <div
+          onClick={() => handleNavMenuClick("Services")}
+          className={activeItem === "Services" ? "active" : ""}
+        >
+          Services
+        </div>
+        <div
+          onClick={() => handleNavMenuClick("Pages")}
+          className={activeItem === "Pages" ? "active" : ""}
+        >
           Pages
           <IoIosArrowDown />
         </div>
-        <div>
+        <div
+          onClick={() => handleNavMenuClick("Blogs")}
+          className={activeItem === "Blogs" ? "active" : ""}
+        >
           Blogs
           <IoIosArrowDown />
         </div>
-        <div>Contact Us</div>
+        <div
+          onClick={() => handleNavMenuClick("Contact Us")}
+          className={activeItem === "Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </div>
       </div>
       <button className="button--blue">Book Appointment</button>
     </nav>
